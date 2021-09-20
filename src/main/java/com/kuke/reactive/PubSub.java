5f27b7f9c7dd2877729a33c440b668d25b51366c
SongHeeJae <gmlwo308@gmail.com>
2021-09-20 21:15:13 +0900

    publisher/subscriber sample

---
+++ src/main/java/com/kuke/reactive/PubSub.java
@@ -0,0 +1,91 @@
+package com.kuke.reactive;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.Flow.*;
+
+public class PubSub {
+    // Publisher <- Observable
+    // Subscriber <- Observer
+
+    public static void main(String[] args) throws InterruptedException {
+        Iterable<Integer> iter = Arrays.asList(1, 2, 3, 4, 5);
+        ExecutorService es = Executors.newSingleThreadExecutor();
+
+        Publisher p = new Publisher() {
+            @Override
+            public void subscribe(Subscriber subscriber) {
+
+                Iterator<Integer> it = iter.iterator();
+                subscriber.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {
+                        es.execute(() -> {
+                            int i = 0;
+                            try {
+                                while (i++ < n) {
+                                    if (it.hasNext()) {
+                                        subscriber.onNext(it.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
+                                }
+                            } catch (RuntimeException e) {
+                                e.printStackTrace();
+                                subscriber.onError(e);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
+
+        Subscriber<Integer> s = new Subscriber<Integer>() {
+            Subscription subscription;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println("onSubscribe");
+                this.subscription = subscription;
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                // 여기서 버퍼 사이즈 같은거로 다음꺼
+                // 이어서 호출할지 동작하는 코드 구현 가능
+                System.out.println(Thread.currentThread().getName() + " onNext, item = " + item);
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+
+        p.subscribe(s);
+
+        es.awaitTermination(10, TimeUnit.HOURS);
+        es.shutdown();
+
+
+    }
+}
