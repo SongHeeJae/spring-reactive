967f2b483166466788c075e6834df2a5d7a55639
SongHeeJae <gmlwo308@gmail.com>
2021-09-25 12:54:59 +0900

    reactor example

---
+++ src/main/java/com/kuke/reactive/operator/ReactorEx.java
@@ -0,0 +1,21 @@
+package com.kuke.reactive.operator;
+
+import reactor.core.publisher.Flux;
+
+public class ReactorEx {
+
+    public static void main(String[] args) {
+        Flux
+                .<Integer>create(e -> {
+                    e.next(1);
+                    e.next(2);
+                    e.next(3);
+                    e.complete();
+                }).log() // create log
+                .map(s -> s * 10)
+                .log() // map log
+                .reduce(0, (a, b) -> a + b)
+                .log() // reduce log
+                .subscribe(System.out::println);
+    }
+}
