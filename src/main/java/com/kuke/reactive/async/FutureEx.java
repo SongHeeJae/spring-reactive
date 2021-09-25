b3e92eedd3c65c0dc63737b4cdd4b69ce4dbe3e7
SongHeeJae <gmlwo308@gmail.com>
2021-09-26 01:01:17 +0900

    비동기 결과 Future, Callback example

--- src/main/java/com/kuke/reactive/async/FutureEx.java
+++ src/main/java/com/kuke/reactive/async/FutureEx.java
@@ -2,6 +2,7 @@ package com.kuke.reactive.async;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
 import java.util.concurrent.*;
 
 @Slf4j
@@ -15,19 +16,56 @@ public class FutureEx {
     // 요청이 들어오면 만들거나, 만들어져있는 스레드를 사용
 
     // Callable : Runnable 과 다르게 return, throw exception
+
+    interface SuccessCallback {
+        void onSuccess(String result);
+    }
+
+    interface ExceptionCallback {
+        void onError(Throwable t);
+    }
+
+    public static class CallbackFutureTask extends FutureTask<String> {
+        SuccessCallback sc;
+        ExceptionCallback ec;
+        public CallbackFutureTask(Callable<String> callable, SuccessCallback sc, ExceptionCallback ec) {
+            super(callable);
+            this.sc = Objects.requireNonNull(sc);
+            this.ec = Objects.requireNonNull(ec);
+        }
+
+        @Override
+        protected void done() {
+            try {
+                sc.onSuccess(get());
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            } catch (ExecutionException e) {
+                ec.onError(e.getCause());
+            }
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService es = Executors.newCachedThreadPool();
 
-        Future<String> f = es.submit(() -> {
+        CallbackFutureTask f = new CallbackFutureTask(() -> {
             Thread.sleep(2000);
+            if (1 == 1) throw new RuntimeException("ERROR");
             log.info("Async");
             return "Hello";
-        });
+        },
+                System.out::println,
+                e -> System.out.println("e.getMessage() = " + e.getMessage())
+        );
 
-        System.out.println(f.isDone()); // 즉시 리턴. non-blocking
-        Thread.sleep(2100);
-        log.info("Exit");
-        System.out.println(f.isDone());
-        System.out.println(f.get()); // blocking, 만약 결과가 끝나지 않았을 때 null같은게 오면 non-blocking
+        es.execute(f);
+        es.shutdown();
+
+//        System.out.println(f.isDone()); // 즉시 리턴. non-blocking
+//        Thread.sleep(2100);
+//        log.info("Exit");
+//        System.out.println(f.isDone());
+//        System.out.println(f.get()); // blocking, 만약 결과가 끝나지 않았을 때 null같은게 오면 non-blocking
     }
 }
